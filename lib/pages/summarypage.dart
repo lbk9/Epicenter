b35b245d510cb97b43a2f6ee769a4f464f9311c6
Liam <liam.keogh@zonal.co.uk>
2020-04-17 13:19:35 +0100

    improved UI, just need to add pictures and improve code maintainability

--- lib/pages/summarypage.dart
+++ lib/pages/summarypage.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:http/http.dart';
 import 'dart:convert';
+import 'package:google_fonts/google_fonts.dart';
 
 class Summary extends StatefulWidget {
   @override
@@ -17,30 +19,40 @@ class _SummaryState extends State<Summary> {
   String globalRecovered;
 
   fetchWorldWideData() async {
-    Response response = await get('https://corona.lmao.ninja/all');
+    Response response = await get('https://corona.lmao.ninja/v2/all');
     worldData = json.decode(response.body);
     setState(() {
-      globalCases = 'Global Cases ${worldData['cases']}';
-      globalDeaths = 'Global Deaths ${worldData['deaths']}';
-      globalRecovered = 'Global Recovered ${worldData['recovered']}';
+      globalCases = '${worldData['cases']}';
+      globalDeaths = '${worldData['deaths']}';
+      globalRecovered = '${worldData['recovered']}';
     });
   }
 
-  List countryData;
+  List affectedCountryData;
   String mostAffectedCountry;
-  fetchCountryData() async {
-    Response response = await get('https://corona.lmao.ninja/countries?sort=cases');
-    countryData = json.decode(response.body);
+  fetchMostAffected() async {
+    Response response = await get('https://corona.lmao.ninja/v2/countries?sort=cases');
     setState(() {
-      countryData = json.decode(response.body);
-      mostAffectedCountry = 'Most affected country ${countryData[0]['country']}';
+      affectedCountryData = json.decode(response.body);
+      mostAffectedCountry = '${affectedCountryData[0]['country']}';
+    });
+  }
+
+  List recoveredCountryData;
+  String mostRecoveredCountry;
+  fetchMostRecovered() async {
+    Response response = await get('https://corona.lmao.ninja/v2/countries?sort=recovered');
+    setState(() {
+      recoveredCountryData = json.decode(response.body);
+      mostRecoveredCountry = '${recoveredCountryData[0]['country']}';
     });
   }
 
     @override
     void initState() {
       fetchWorldWideData();
-      fetchCountryData();
+      fetchMostAffected();
+      fetchMostRecovered();
       super.initState();
     }
 
@@ -50,38 +62,38 @@ class _SummaryState extends State<Summary> {
       body: Container(
         child: Column(
           children: <Widget>[
-            FractionallySizedBox(
-              widthFactor: 1,
-              child: ClipRRect(
+            ClipRRect(
                 clipBehavior: Clip.antiAlias,
                 borderRadius: BorderRadius.only(
                     bottomRight: Radius.elliptical(150, 70),
                     bottomLeft: Radius.elliptical(150, 70)),
                 child: Container(
                   color: Colors.blue,
+                  width: double.infinity,
                   height: 200,
                   child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                          'Stay Home'
-                      ),
-                      Text(
-                          'Save Lives'
-                      )
-                    ],
-                  )
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                            'Stay Home'
+                        ),
+                        Text(
+                            'Save Lives'
+                        )
+                      ],
+                    ),
                 ),
               ),
-            ),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Text(
                 'Common Symptoms',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold
+                style: GoogleFonts.lato(
+                  textStyle: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold
+                  )
                 ),
               ),
             ),
@@ -106,50 +118,152 @@ class _SummaryState extends State<Summary> {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.only(left: 15, top: 10, right: 15, bottom: 10),
+              padding: const EdgeInsets.only(left: 10, top: 10, right: 10, bottom: 10),
               child: Card(
-                color: Colors.grey,
+                elevation: 15,
+                color: Colors.grey[200],
                 child: Container(
                   height: 250,
                   child: Column(
                     children: <Widget>[
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          Text(
-                            mostAffectedCountry,
-                            style: TextStyle(
-                              fontSize: 18,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 60,
-                            width: 60,
-                            child: Image.network(
-                              countryData[0]['countryInfo']['flag']
-                            ),
-                          )
-                        ],
+                      Padding(
+                        padding: const EdgeInsets.only(top: 5.0),
+                        child: Text(
+                          'Total Cases',
+                          style: GoogleFonts.lato(
+                              textStyle: TextStyle(
+                                fontSize: 20
+                              )),
+                        ),
                       ),
                       Text(
                         globalCases,
-                        style: TextStyle(
-                            fontSize: 18,
-                          color: Colors.yellow
+                        style: GoogleFonts.lato(
+                          textStyle: TextStyle(
+                              fontSize: 20,
+                              color: Colors.amber[800],
+                              fontWeight: FontWeight.bold
+                          )
                         ),
                       ),
-                      Text(
-                          globalDeaths,
-                        style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.red
+                      Padding(
+                        padding: const EdgeInsets.only(top: 15.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            Column(
+                                children: <Widget>[
+                                  Text(
+                                    'Total Deaths',
+                                    style: GoogleFonts.lato(
+                                        textStyle: TextStyle(
+                                          fontSize: 20
+                                        )
+                                    ),
+                                  ),
+                                  Text(
+                                      globalDeaths,
+                                      style: GoogleFonts.lato(
+                                          textStyle: TextStyle(
+                                            fontSize: 20,
+                                            color: Colors.red,
+                                            fontWeight: FontWeight.bold
+                                          )
+                                      )
+                                  )]
+                            ),
+                            Column(
+                                children: <Widget>[
+                                  Text(
+                                    'Total Recoveries',
+                                    style: GoogleFonts.lato(
+                                        textStyle: TextStyle(
+                                          fontSize: 20
+                                        )
+                                    ),
+                                  ),
+                                  Text(
+                                      globalRecovered,
+                                      style: GoogleFonts.lato(
+                                          textStyle: TextStyle(
+                                              fontSize: 20,
+                                              color: Colors.green,
+                                              fontWeight: FontWeight.bold
+                                          )
+                                      )
+                                  )]
+                            )
+                          ],
                         ),
                       ),
-                      Text(
-                          globalRecovered,
-                        style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.green
+                      Padding(
+                        padding: const EdgeInsets.only(top: 15),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            Column(
+                              children: <Widget>[
+                                Text(
+                                  'Most Cases',
+                                  style: GoogleFonts.lato(
+                                      textStyle: TextStyle(
+                                          fontSize: 20
+                                      )
+                                  )
+                                ),
+                                Container(
+                                  width: 100,
+                                  height: 70,
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                      fit: BoxFit.cover,
+                                      image: NetworkImage(affectedCountryData[0]['countryInfo']['flag'])
+                                    ),
+                                    borderRadius: BorderRadius.all(Radius.circular(5)),
+                                  ),
+                                ),
+                                Text(
+                                  mostAffectedCountry,
+                                  style: GoogleFonts.lato(
+                                      textStyle: TextStyle(
+                                          fontSize: 14
+                                      )
+                                  ),
+                                )
+                              ],
+                            ),
+                            Column(
+                              children: <Widget>[
+                                Text(
+                                    'Most Recoveries',
+                                    style: GoogleFonts.lato(
+                                        textStyle: TextStyle(
+                                            fontSize: 20
+                                        )
+                                    )
+                                ),
+                                Container(
+                                  width: 100,
+                                  height: 70,
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                        fit: BoxFit.cover,
+                                        image: NetworkImage(recoveredCountryData[0]['countryInfo']['flag'])
+                                    ),
+                                    borderRadius: BorderRadius.all(Radius.circular(5)),
+                                  ),
+                                ),
+                                Text(
+                                  mostRecoveredCountry,
+                                  style: GoogleFonts.lato(
+                                      textStyle: TextStyle(
+                                          fontSize: 14
+                                      )
+                                  ),
+                                )
+                              ],
+                            )
+                          ],
                         ),
                       ),
                     ],
