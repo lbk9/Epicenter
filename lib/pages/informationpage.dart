5609528b7921a02bdcc6a6fc488dabdea588581a
Liam <liam.keogh@zonal.co.uk>
2020-04-21 09:39:18 +0100

    filled info page with symptoms and advice

--- lib/pages/informationpage.dart
+++ lib/pages/informationpage.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -7,6 +8,11 @@ class Information extends StatefulWidget {
 }
 
 class _InformationState extends State<Information> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,21 +24,157 @@ class _InformationState extends State<Information> {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Text(
-                'Common Symptoms',
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                'STAY HOME SAVE LIVES',
                 style: GoogleFonts.lato(
-                  fontSize: 22,
+                  fontSize: 24,
                   fontWeight: FontWeight.bold,
+                  color: Colors.black87
                 ),
               ),
-          Text(
-            'There are a number of common symptoms of COVID-19 such as;',
-            style: GoogleFonts.lato(
-                fontSize: 13,
-                fontStyle: FontStyle.italic,
-                color: Colors.grey[500]
             ),
-          )
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0, top: 8.0, right: 8.0, bottom: 0.0),
+            child: Text(
+              'Common Symptoms',
+              style: GoogleFonts.lato(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0, top: 0, right: 8.0, bottom: 0.0),
+            child: Text(
+              'There are a number of common symptoms of COVID-19 such as;',
+              style: GoogleFonts.lato(
+                  fontSize: 14,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.grey[600]
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(left: 15),
+                  child: Text(
+                    'A high temperature. Hot to the touch on your chest or back',
+                    style: GoogleFonts.lato(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15, top: 5),
+                  child: Text(
+                      'A new, continuous cough. Meaning 3 or more coughiung episodes in 24 hours',
+                    style: GoogleFonts.lato(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15, top: 5),
+                  child: Text(
+                      'Some have noted a loss in smell and taste',
+                    style: GoogleFonts.lato(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'How can you prevent it?',
+              style: GoogleFonts.lato(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0, top: 0, right: 8.0, bottom: 0.0),
+            child: Text(
+              'The best ways to defend yourself against this pandemic;',
+              style: GoogleFonts.lato(
+                  fontSize: 14,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.grey[600]
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(left: 15),
+                  child: Text(
+                    'Wash your hands with soap and water often – for at least 20 seconds',
+                    style: GoogleFonts.lato(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15, top: 5),
+                  child: Text(
+                    'Wash your hands as soon as you get home',
+                    style: GoogleFonts.lato(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15, top: 5),
+                  child: Text(
+                    'Cover your mouth and nose with a tissue when you cough or sneeze',
+                    style: GoogleFonts.lato(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15, top: 5),
+                  child: Text(
+                    'Put used tissues in the bin immediately and wash your hands',
+                    style: GoogleFonts.lato(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15, top: 5),
+                  child: Text(
+                    'Not touch your face if your hands are not clean',
+                    style: GoogleFonts.lato(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
       bottomNavigationBar: new BottomAppBar(
